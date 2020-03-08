ed72cc0548ed524b0e485944af972d0c312175bd
M. Rachmat Gunawan <rachmatg@yahoo.com>
2020-03-08 21:22:22 +0700

    Update settings.py

--- klinik/settings.py
+++ klinik/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'klinik.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'vdn',                      # Or path to database file if using sqlite3.
-        'USER': 'puspen',                      # Not used with sqlite3.
-        'PASSWORD': 'puspen123',                  # Not used with sqlite3.
+        'NAME': 'xxxx',                      # Or path to database file if using sqlite3.
+        'USER': 'xxxx',                      # Not used with sqlite3.
+        'PASSWORD': 'xxxx',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
